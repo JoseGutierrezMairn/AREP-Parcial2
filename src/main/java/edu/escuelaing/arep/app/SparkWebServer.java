2ce407c915024b0d23176071cf4fcef6a678435f
Jose Gutierrez Marin <andres.gutierrez-m@mail.escuelaing.edu.co>
2021-03-24 14:22:09 -0500

    sparkWeb and calculator

---
+++ src/main/java/edu/escuelaing/arep/app/SparkWebServer.java
@@ -0,0 +1,42 @@
+package edu.escuelaing.arep.app;
+
+import static spark.Spark.*;
+
+import spark.Request;
+import spark.Response;
+/**
+ * Hello world!
+ *
+ */
+public class SparkWebServer 
+{
+	 private static Calculadora ca = new Calculadora();
+
+
+	    public static void main(String ... args){
+	        port(getPort());
+	        get("/hello", (req,res) -> "Hello!" );
+	        get("/sin", SparkWebServer::getSin);
+	        get("/cos",SparkWebServer::getCos);
+
+	    }
+
+
+	    private static Object getSin(Request request, Response response) {
+
+	        return null;
+	    }
+
+
+	    private static Object getCos(Request request, Response response) {
+
+	        return null;
+	    }
+
+	    private static int getPort() {
+	        if (System.getenv("PORT") != null) {
+	            return Integer.parseInt(System.getenv("PORT"));
+	        }
+	        return 5000;
+	    }
+}
